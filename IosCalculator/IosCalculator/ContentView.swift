5a129a8f49eab1acd1f4eb1eaf2fd64505e62290
1pedr1n <pedrinfnn@gmail.com>
2023-12-19 03:26:10 -0300

    line 2

--- IosCalculator/IosCalculator/ContentView.swift
+++ IosCalculator/IosCalculator/ContentView.swift
@@ -9,21 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing:0) {
             HStack{
                 Button("7"){}
                     .padding()
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .background(Color.red)
+                   
                 Button("8"){}
                     .padding()
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .background(Color.pink)
-            }
+                   
+                Button("9"){}
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    
+                Button("X"){}
+                    .padding()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .background(.orange)
+                  
+            }.foregroundColor(.white)
+            HStack{
+                Button("4"){}
+                    .padding()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                   
+                Button("5"){}
+                    .padding()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                   
+                Button("6"){}
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    
+                Button("-"){}
+                    .padding()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .background(.orange)
+                  
+            }.foregroundColor(.white)
             
-        }
+            
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).background(Color.black)
         
-        .padding()
+       
     }
 }
 
